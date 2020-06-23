d80c81f2098ae5cb91465c0ac4505a1ce7f0ecf5
JM <junma75@gmail.com>
2020-06-23 04:34:08 +0000

    .

--- wbfex/spread_monitor.py
+++ wbfex/spread_monitor.py
@@ -16,18 +16,30 @@ def handle_simple(channel, data):
             print( '[trade]', datetime.datetime.fromtimestamp( float(d['timestamp'])/1000), di['side'], d['symbol'], di['price'], di['vol'], di['amount'])
 
 trade_price = 0
+RISE_PRICE_CAP=-1
+RISE_FACTOR=-1
+
 def price_control_rise(channel,data):
     global trade_price
-    factor = 10/100
+    factor = 3/100
+
     if 'depth' in channel:
         bids = data['bids']
         asks = data['asks']
         if trade_price >0:
-            print( asks )
-            sub_asks = filter(lambda e:float(e[0])<trade_price*(1+factor), asks)
-            print( list(sub_asks) )
-            sub_total = reduce(lambda a,b: a[0]*a[1]+b[0]*b[1], sub_asks)
-            print(sub_total)
+            if RISE_FACTOR:
+                sub_asks = filter(lambda e:float(e[0])<trade_price*(1+float(RISE_FACTOR)/100), asks)
+            elif RISE_PRICE_CAP:
+                sub_asks = filter(lambda e:float(e[0])<float(RISE_PRICE_CAP), asks)
+            else:
+                pass
+            sub_asks = map(lambda e: [float(e[0]), float(e[1])], sub_asks )
+            t, v = 0,0
+            for e in sub_asks:
+                t += e[0]*e[1]
+                v += e[1]
+            print( t, trade_price*(1+factor) )
+
     elif 'trade' in channel:
         for d in data:
             di = d['info']
@@ -40,7 +52,12 @@ Trail run
 """""""""""""""""""""""""""""""""
 @click.command()
 @click.option('--symbols','symbols',help='Comma-sperated symbols to be monitored.')
-def main(symbols):
+@click.option('--rise_cap', 'rise_cap', help='rise cap')
+@click.option('--rise_factor', 'rise_factor', help='rise factor')
+def main(symbols, rise_cap, rise_factor):
+    global RISE_FACTOR,RISE_PRICE_CAP
+    RISE_PRICE_CAP = rise_cap
+    RISE_FACTOR = rise_factor
     wbf_ws = WBFExWebsocket(on_update_trade=price_control_rise, on_update_depth=price_control_rise, ws_symbol=symbols.split(','))
     wbf_ws.start()
 
