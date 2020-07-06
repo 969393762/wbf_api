c215f487de0172846b14ef6f7d367d2cbc64716b
Jun Ma <junma018@outlook.com>
2020-07-06 14:40:19 +0800

    Extract cls method in rest apis.

--- wbfex/ut/test_rest.py
+++ wbfex/ut/test_rest.py
@@ -13,26 +13,38 @@ class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.rest = WBFExRest(None,None)
-        self.oneminute = self.rest.get_ohlcv_data('BTC/USDT')
+        self.symbol = 'BTC/USDT'
+        self.onehour = WBFExRest.get_ohlcv_data(self.symbol, 60)
 
     def tearDown(self):
         pass
 
 
     def test_ohlcv(self):
-        self.assertIsNotNone(self.oneminute, 'Should not be none.')
-        self.assertEqual(len(self.oneminute), 300, 'Default length is 300 in not empty.')
+        self.assertIsNotNone(self.onehour, 'Should not be none.')
+        self.assertEqual(len(self.onehour), 300, 'Default length is 300 in not empty.')
     
     def test_ohlcv_timestamp(self):
-        ts = self.oneminute[0][0]
+        ts = self.onehour[0][0]
         self.assertEqual(len(str(ts)), 10, 'Timestamp should be a 10-digit number.')  
     
     def test_ohlcv_numeric(self):
-        _, o,h,l,c,_ = self.oneminute[0]
+        _, o,h,l,c,_ = self.onehour[0]
         self.assertTrue(h>=l, 'High should be no less than low.')
         self.assertTrue(o<=h, 'Open should be no greater than high.')
         self.assertTrue(c<=h, 'Close should be no greater than high.')
+        
+    def test_ohlcv_timeframe(self):
+        ts0,_,_,_,_,_ = self.onehour[0]
+        ts1,_,_,_,_,_ = self.onehour[1]
+        self.assertEqual(ts1-ts0, 3600, "1-hour candlestick lines should have a gap of 3600 seconds, or 1 hour.")
+        
+    def test_ticker_data(self):
+        self.ticker = self.rest.get_ticker_data(self.symbol)
+        self.assertTrue( all(map(lambda s: s in self.ticker.keys(),['ask','bid', 'price','amount','symbol'] )), 'Missing keys in the return values.')
+        
+        bid,ask = self.ticker['bid'], self.ticker['ask']
+        self.assertTrue(ask>bid, 'Ask price must be higher than bid price.')
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
